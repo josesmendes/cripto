d8b6d275c1a9ef8d82ae8a24cd92b08bc37c13c8
Paulo Henrique Pianissola de Cerqueira <paulophpc@gmail.com>
2024-03-30 13:26:01 +0000

    final EX1

--- TP2/EX1.py
+++ TP2/EX1.py
@@ -1,8 +1,21 @@
-p = pow(2,255) - 19
+#p = pow(2,255) - 19
 #d = -121665/121666
-
-d = (-121665 * pow(121666,-1, p)) % p
+p = (pow(2, 448) - pow(2, 224)) - 1
+d = (-39081 * pow(39080,-1, p)) % p
 
 print("P: ", p)
 print("D: ", d)
 
+
+u=9
+x=15112221349535400772501151409588531511454012693041857206046113283949847762202
+y=((u-1) * pow(u+1, -1, p)) % p
+
+
+
+
+curve_equation = ((-1 * x*x + y*y) % p)
+curve_equation2 = ((1 * d * pow(x,2) * pow(y,2)) % p) 
+
+print("CURVA: ", curve_equation)
+print("CURVA2: ", curve_equation2)
